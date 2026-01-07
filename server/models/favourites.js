96b00c2d948a0e4ad62356ccd077188e85c9d931
skipd <skipdevs@gmail.com>
2026-01-07 17:29:48 +0300

    onDelete: 'CASCADE'

--- server/models/favourites.js
+++ server/models/favourites.js
@@ -1,9 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-  const Favorites = sequelize.define("Favorites", {});
+  const Favorites = sequelize.define("Favorites", {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', // Reference Users table
+        key: 'id'
+      }
+    },
+    videoId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Videos', // Reference Videos table
+        key: 'id'
+      }
+    },
+  });
+
 
   Favorites.associate = (models) => {
-    Favorites.belongsTo(models.Users, { foreignKey: "userId", as: "user" });
-    Favorites.belongsTo(models.Videos, { foreignKey: "videoId", as: "video" });
+    Favorites.belongsTo(models.Users, { foreignKey: "userId", as: "user",onDelete: 'CASCADE' });
+    Favorites.belongsTo(models.Videos, { foreignKey: "videoId", as: "video", onDelete: 'CASCADE' });
   };
 
   return Favorites;
